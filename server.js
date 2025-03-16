dc688e2c7fe73253971e10b4687b476f8a0da366
martins0023 <martinsmiracle45@gmail.com>
2025-03-16 15:06:37 +0100

    fix volunteer request

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const consultationSchema = new mongoose.Schema({
 
 const Consultation = mongoose.model("Consultation", consultationSchema);
 
+
 // define mongoose for sponsorship
 const sponsorSchema = new mongoose.Schema({
   name: String,
@@ -43,6 +44,7 @@ const sponsorSchema = new mongoose.Schema({
 
 const Sponsor = mongoose.model("Sponsor", sponsorSchema)
 
+//contact connection
 const ContactSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -54,6 +56,68 @@ const ContactSchema = new mongoose.Schema({
 });
 
 const Contact = mongoose.model("Contact", ContactSchema);
+
+//volunteer connection
+const VolunteerSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Volunteer = mongoose.model("Volunteer", ContactSchema);
+
+//volunteer endpoint
+app.post("/api/volunteer", async (req, res) => {
+  console.log("Received POST /api/volunteer with body:", req.body);
+  try {
+    // 1. Save data to MongoDB
+    const volunteerData = req.body;
+    const newVolunteer = new Volunteer(volunteerData);
+    await newVolunteer.save();
+    console.log("Saved volunteer Information:", newVolunteer);
+
+    // 2. Send confirmation email to the user
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // e.g. your_gmail@gmail.com
+        pass: process.env.EMAIL_PASS, // an app password if using 2FA
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: volunteerData.email,
+      subject: "Your volunteer request has been received!",
+      text: `Dear ${volunteerData.firstName},
+  
+  We’ve received your message of becoming part of our volunteers. One of our team members will get back to you soon.
+  
+  Here’s a summary of your request:
+  - Name: ${volunteerData.firstName} ${volunteerData.lastName}
+  - Phone: ${volunteerData.phone}
+  - Message: ${volunteerData.message}
+  
+  Thank you for reaching out to becoming a volunteer with us!
+  
+  Best Regards,
+  Doctor Kays Team`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent info:", info);
+
+    res
+      .status(200)
+      .json({ message: "Volunteer details saved and email sent successfully" });
+  } catch (err) {
+    console.error("Error saving volunteer details or sending email:", err);
+    res.status(500).json({ error: "Error saving volunteer details or sending email" });
+  }
+});
 
 //contact endpoint
 app.post("/api/contact", async (req, res) => {
